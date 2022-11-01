5016eecee32db14fdab212c7ff3ef786edd9a319
Daksh2356 <dakshmakhija@gmail.com>
2022-11-01 20:47:50 +0530

    react app setup and routing done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
