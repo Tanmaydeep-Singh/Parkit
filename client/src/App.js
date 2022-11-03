a131f2215893f99b389fc1a0da0e390bf567298c
Daksh2356 <dakshmakhija@gmail.com>
2022-11-03 16:30:10 +0530

    corrected the route path for sign in page and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import Signin from "./pages/Signin_page";
 import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    
     <Routes>
-      <Route path="/login" element={<Signin />} />
-      <Route path="/footer" element={<Footer />} />
-      
+      <Route path="/" element={<Signin />} />
     </Routes>
   );
 }
--- client/src/pages/Signin_page.jsx
+++ client/src/pages/Signin_page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar/index";
 
 const Signin = () => {
   return (
     <>
-    
       <Navbar />
+      <Footer />
     </>
   );
 };
