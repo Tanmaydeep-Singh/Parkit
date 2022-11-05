1177c67ac4b2e5f1ec6ff320a270a0bae3b084f3
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-11-05 13:44:26 +0530

    Profile page

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -6,8 +6,8 @@ import { AiFillSetting} from "react-icons/ai";
 const Bookings = () => {
   return (
     <>
-      <div className="flex m-0 my-8">
-        <div className="flex flex-col m-0 py-4 w-1/3  bg-gray-400 justify-evenly rounded-md ">
+      <div className="flex  p-14">
+        <div className="flex flex-col m-0 py-4 w-1/3  bg-gray-400 justify-center rounded-md ">
           <div className="flex my-5 align-center justify-center flex-wrap gap-2 text-xl hover:bg-gray-200">
             <FaUserAlt className="m-auto mx-0" /> ORDERS
           </div>
@@ -21,16 +21,18 @@ const Bookings = () => {
             <FaSignOutAlt className="m-auto mx-0" /> Logout 
           </div>
         </div>
+
+
         <div className="flex flex-col m-4 p-4 w-2/3">
           <h3 className="font-semibold tracking-normal text-lg">
             PAST BOOKINGS
           </h3>
-          <div className="flex border border-black p-4">
+          <div className="flex border border-black p-4 rounded-md m-4">
             <div className="flex w-1/3 flex-col gap-1">
               <img
                 src=""
                 alt="LOGO"
-                className="w-full h-full drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -39,14 +41,18 @@ const Bookings = () => {
               </div>
               <h5>Address : abcd </h5>
               <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+<button type="button" className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2">Book Again</button>
+
             </div>
           </div>
-          <div className="flex border border-black p-4">
+          <div className="flex border border-black p-4 rounded-md m-4">
             <div className="flex w-1/3 flex-col gap-1">
               <img
                 src=""
                 alt="LOGO"
-                className="w-full h-full drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -55,14 +61,17 @@ const Bookings = () => {
               </div>
               <h5>Address : abcd </h5>
               <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+              <button type="button" className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2">Book Again</button>
             </div>
           </div>
-          <div className="flex border border-black p-4">
+          <div className="flex border border-black p-4 rounded-md m-4">
             <div className="flex w-1/3 flex-col gap-1">
               <img
                 src=""
                 alt="LOGO"
-                className="w-full h-full drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -71,14 +80,18 @@ const Bookings = () => {
               </div>
               <h5>Address : abcd </h5>
               <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+<button type="button" className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2">Book Again</button>
+
             </div>
           </div>
-          <div className="flex border border-black p-4">
+          <div className="flex border border-black p-4 rounded-md m-4">
             <div className="flex w-1/3 flex-col gap-1">
               <img
                 src=""
                 alt="LOGO"
-                className="w-full h-full drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -87,14 +100,18 @@ const Bookings = () => {
               </div>
               <h5>Address : abcd </h5>
               <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+<button type="button" className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2">Book Again</button>
+
             </div>
           </div>
-          <div className="flex border border-black p-4">
+          <div className="flex border border-black p-4  rounded-md m-4">
             <div className="flex w-1/3 flex-col gap-1">
               <img
                 src=""
                 alt="LOGO"
-                className="w-full h-full drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -103,6 +120,10 @@ const Bookings = () => {
               </div>
               <h5>Address : abcd </h5>
               <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+<button type="button" className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2">Book Again</button>
+
             </div>
           </div>
         </div>
