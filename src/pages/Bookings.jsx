c9576e7718a71d1bc8fff53ddeb7ff4fcc3dc60b
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-11-05 22:08:52 +0530

    ui

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -178,7 +178,9 @@ const BookingsLg = () => {
   return (
     <>
       <div className="flex">
-        <div className=" bg-gray-400 justify-center rounded-md ">
+      <div className="px-6 fixed h-80vh bg-gray-500 rounded-tr-md rounded-br-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 border border-l-0 border-gray-100 justify-center rounded-md ">
+
+        {/* <div className="px-6 fixed h-80vh bg-white rounded-tr-md rounded-br-md border-l-0 border-gray-100 justify-center rounded-md "> */}
           <div className="flex m-5 align-center justify-center flex-wrap gap-2 text-lg hover:bg-gray-200">
             <FaUserAlt className="m-auto mx-0" /> ORDERS
           </div>
@@ -196,7 +198,7 @@ const BookingsLg = () => {
           </div>
         </div>
 
-        <div className="flex flex-col m-4 p-4 w-2/3">
+        <div className="flex flex-col m-4 p-4 w-2/3  text-white justify-self-center mx-auto ">
           <h3 className="font-semibold tracking-normal text-lg">
             PAST BOOKINGS
           </h3>
@@ -205,7 +207,31 @@ const BookingsLg = () => {
               <img
                 src=""
                 alt="LOGO"
-                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300 rounded"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="flex items-start justify-between">
+                <h3 className="text-xl font-semibold">Location_Name</h3>
+              </div>
+              <h5>Address : abcd </h5>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+              <p>Booking ID : 01ss1938912jh1i2j12</p>
+
+              <button
+                type="button"
+                className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2 rounded"
+              >
+                Book Again
+              </button>
+            </div>
+          </div>
+          <div className="flex border border-black p-4 rounded-md m-4">
+            <div className="flex w-1/3 flex-col gap-1">
+              <img
+                src=""
+                alt="LOGO"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300 rounded"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -229,7 +255,7 @@ const BookingsLg = () => {
               <img
                 src=""
                 alt="LOGO"
-                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300 rounded"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -242,7 +268,7 @@ const BookingsLg = () => {
 
               <button
                 type="button"
-                className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2"
+                className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2 "
               >
                 Book Again
               </button>
@@ -253,31 +279,7 @@ const BookingsLg = () => {
               <img
                 src=""
                 alt="LOGO"
-                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <div className="flex items-start justify-between">
-                <h3 className="text-xl font-semibold">Location_Name</h3>
-              </div>
-              <h5>Address : abcd </h5>
-              <p>Booking ID : 01ss1938912jh1i2j12</p>
-              <p>Booking ID : 01ss1938912jh1i2j12</p>
-
-              <button
-                type="button"
-                className="text-white bg-blue-600  focus:ring-blue-300 font-medium rounded-md text-xs px-5 py-2.5 text-center mr-0 md:mr-0 w-1/2"
-              >
-                Book Again
-              </button>
-            </div>
-          </div>
-          <div className="flex border border-black p-4 rounded-md m-4">
-            <div className="flex w-1/3 flex-col gap-1">
-              <img
-                src=""
-                alt="LOGO"
-                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1  drop-shadow-md border bg-gray-200 border-gray-300 rounded"
               />
             </div>
             <div className="flex flex-col gap-1">
@@ -301,7 +303,7 @@ const BookingsLg = () => {
               <img
                 src=""
                 alt="LOGO"
-                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300"
+                className="w-40 h-40 aspect-ratio: 1 / 1 drop-shadow-md border bg-gray-200 border-gray-300 rounded"
               />
             </div>
             <div className="flex flex-col gap-1">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ function Home() {
   return (
     <div>
 
-      <div className=" h-547.2   bg-gradient-to-r from-cyan-500 to-blue-500" >
+      <div className=" h-547.2 bg-gray-700 rounded-bl-md rounded-br-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 border-b-1 border-gray-100 " >
 
 
         <h1 className="align-bottom" >Most Popular Providers.</h1>
       </div>
 
-<div className="w-11/12 m-auto  grid grid-cols-5">
+<div className="w-11/12 m-auto  grid grid-cols-1 lg:grid-cols-5 lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-2">
 <ProviderCard img='https://picsum.photos/200/300' name="Tanmaydeep" walletAddress="1234567890" location="here" />
 <ProviderCard img='https://picsum.photos/200/300' name="Tanmaydeep" walletAddress="1234567890" location="here" />
 <ProviderCard img='https://picsum.photos/200/300' name="Tanmaydeep" walletAddress="1234567890" location="here" />
